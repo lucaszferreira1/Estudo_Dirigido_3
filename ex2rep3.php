ac025ea49d7018b627327ee98d4006a65e3a4f3f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-05-14 10:57:49 -0300

    Resultado Final

--- ex2rep3.php
+++ ex2rep3.php
@@ -1,1 +1,22 @@
+
 <?php
+$n=1;
+print "Para parar digite 0\n";
+
+while ($n<>0){
+print "Digite um número:";
+$n=(float)fgets(STDIN);
+
+if ($n<>0){
+
+if ($n%2==0){
+    print "Número par\n";
+}
+elseif ($n%2==1){
+    print "Número ímpar\n";
+}
+}
+elseif ($n==0){
+    print "Número nulo\n";
+}
+}
