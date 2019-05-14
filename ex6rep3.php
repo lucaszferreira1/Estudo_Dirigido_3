e39b14e8e13fd8a59972b75f92b669f08dabf702
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-05-14 10:59:41 -0300

    Resultado Final

--- ex6rep3.php
+++ ex6rep3.php
@@ -1,1 +1,21 @@
 <?php
+
+$numerosnegativos=0;
+$numerospositivos=0;
+$n=1;
+
+print "Digite 0 para terminar\n";
+
+while ($n<>0){
+   print "Escreva um número:";
+   $n=(float)fgets(STDIN);
+   if ($n<0){
+       $numerosnegativos=$numerosnegativos+1;
+   }
+   elseif ($n>0){
+       $numerospositivos=$numerospositivos+1;
+   }
+}
+
+print "$numerosnegativos números negativos\n";
+print "$numerospositivos números positivos\n";
